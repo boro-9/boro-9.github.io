ef4efa48de9fba52dbb4f4dc4b44135eabcc8a49
boro <borce1985@gmail.com>
2022-08-30 01:56:49 +0200

    Vezhbi

--- Vezhba - 9 Modal JS/main.js
+++ Vezhba - 9 Modal JS/main.js
@@ -1,66 +1,66 @@
 
-(function(){
+(function () {
 
   let storeItems = document.querySelectorAll('.store-item');
- 
+
   let lightBox = document.querySelector('.lightbox-container');
 
   let lightBoxItem = document.querySelector('.lightbox-item');
 
   let images = document.querySelectorAll('.store-img');
-  
-  
+
+
 
   let imageList = [];
 
   let imageCounter = 0;
 
-  images.forEach(function(image){
+  images.forEach(function (image) {
 
     imageList.push(image.src);
   })
-  
 
-  storeItems.forEach(function(item) {
- 
 
-    item.addEventListener('click', function(e){
+  storeItems.forEach(function (item) {
 
-    let image = e.target.src;
 
-    lightBoxItem.style.backgroundImage = `url(${image})`;
+    item.addEventListener('click', function (e) {
 
-    lightBox.classList.add('show');
+      let image = e.target.src;
 
-    imageCounter = imageList.indexOf(image);
+      lightBoxItem.style.backgroundImage = `url(${image})`;
+
+      lightBox.classList.add('show');
+
+      imageCounter = imageList.indexOf(image);
     });
-  }); 
-  
+  });
+
 
 
   let btnLeft = document.querySelector('.btnLeft');
-  btnLeft.addEventListener('click', function(){
+  btnLeft.addEventListener('click', function () {
     imageCounter--;
-    if (imageCounter < 0){
-      imageCounter = imageList.length -1;
+    if (imageCounter < 0) {
+      imageCounter = imageList.length - 1;
     }
     lightBoxItem.style.backgroundImage = `url(${imageList[imageCounter]})`
   });
 
   let btnRight = document.querySelector('.btnRight');
-  btnRight.addEventListener('click', function(){
+  btnRight.addEventListener('click', function () {
     imageCounter++;
-    if (imageCounter >= imageList.length){
+    if (imageCounter >= imageList.length) {
       imageCounter = 0;
     }
     lightBoxItem.style.backgroundImage = `url(${imageList[imageCounter]})`;
   });
-  
 
-    let lightBoxClose = document.querySelector('.lightbox-close');
-    lightBoxClose.addEventListener('click', function(){
+
+  let lightBoxClose = document.querySelector('.lightbox-close');
+  lightBoxClose.addEventListener('click', function () {
     lightBox.classList.remove('show');
-    });
-  
-  
+  });
+
+
 })();
